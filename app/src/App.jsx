b92efbb986f49fdd4fd04cfbe0bd726d64203075
Filipe Lopes <filiperocklopes@gmail.com>
2024-10-28 22:26:45 +0000

    feat: using vite instead of create-react-app

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./views/Login/index";
 import Register from "./views/Register/index";
