05f0518d1edb2d7dbba591ce2d5472826c7b7770
willsf2021 <willsf2021@gmail.com>
2025-02-25 09:24:24 -0300

    fix(validation register page): add phone number validation to Yup schema Now the phone requires a number with 10 or 11 digits, displaying an error message if the format is incorrect

--- src/views/Register/validationSchema.js
+++ src/views/Register/validationSchema.js
@@ -4,6 +4,10 @@ export default Yup.object({
   username: Yup.string().required("O nome de usuário é obrigatório"),
   name: Yup.string().required("O nome é obrigatório"),
   password: Yup.string().required("A senha é obrigatória"),
+  phone: Yup.string().matches(
+    /^\d{10,11}$/,
+    "O telefone deve ter no máximo 11 dígitos"
+  ),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password"), null], "As senhas precisam ser iguais")
     .required("A confirmação de senha é obrigatória"),
